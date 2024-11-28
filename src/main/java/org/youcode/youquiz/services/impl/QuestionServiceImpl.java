1044c67070bba3777a42d9cefb5aed37cf9141e6
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-29 00:47:45 +0100

    feat: update create method in QuestionServiceImpl: added a verification for the existence of the level

--- src/main/java/org/youcode/youquiz/services/impl/QuestionServiceImpl.java
+++ src/main/java/org/youcode/youquiz/services/impl/QuestionServiceImpl.java
@@ -7,9 +7,11 @@ import org.youcode.youquiz.common.exceptions.EntityNotFoundException;
 import org.youcode.youquiz.common.services.GenericServiceImpl;
 import org.youcode.youquiz.dtos.question.QuestionRequestDTO;
 import org.youcode.youquiz.dtos.question.QuestionResponseDTO;
+import org.youcode.youquiz.entities.Level;
 import org.youcode.youquiz.entities.Question;
 import org.youcode.youquiz.entities.Subject;
 import org.youcode.youquiz.mappers.QuestionMapper;
+import org.youcode.youquiz.repositories.LevelRepository;
 import org.youcode.youquiz.repositories.QuestionRepository;
 import org.youcode.youquiz.repositories.SubjectRepository;
 import org.youcode.youquiz.services.QuestionService;
@@ -20,9 +22,11 @@ import org.youcode.youquiz.services.QuestionService;
 public class QuestionServiceImpl extends GenericServiceImpl<Question, Long, QuestionRequestDTO, QuestionResponseDTO> implements QuestionService {
 
     private final SubjectRepository subjectRepository;
-    public QuestionServiceImpl(QuestionRepository repository, QuestionMapper mapper, SubjectRepository subjectRepository) {
+    private final LevelRepository levelRepository;
+    public QuestionServiceImpl(QuestionRepository repository, QuestionMapper mapper, SubjectRepository subjectRepository, LevelRepository levelRepository) {
         super(repository, mapper);
         this.subjectRepository = subjectRepository;
+        this.levelRepository = levelRepository;
     }
 
     @Override
@@ -30,8 +34,12 @@ public class QuestionServiceImpl extends GenericServiceImpl<Question, Long, Ques
         Subject existingSubject = subjectRepository.findById(requestDto.subjectId())
                 .orElseThrow(() -> new EntityNotFoundException("Subject with Id " + requestDto.subjectId() + " does not exist"));
 
+        Level existingLevel = levelRepository.findById(requestDto.levelId())
+                .orElseThrow(() -> new EntityNotFoundException("Level with Id " + requestDto.levelId() + " does not exist"));
+
         Question question = mapper.toEntity(requestDto);
         question.setSubject(existingSubject);
+        question.setLevel(existingLevel);
         Question saved = repository.save(question);
         return mapper.toDto(saved);
     }
