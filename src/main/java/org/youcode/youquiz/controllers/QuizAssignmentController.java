b881a34eced8ff381aece3e332a2b75d33eba2bd
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-28 23:59:15 +0100

    manage quiz assignments: implement create method in QuizAssignment service and controller

--- src/main/java/org/youcode/youquiz/controllers/QuizAssignmentController.java
+++ src/main/java/org/youcode/youquiz/controllers/QuizAssignmentController.java
@@ -1,7 +1,12 @@
 package org.youcode.youquiz.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.youcode.youquiz.common.ApiResponse;
 import org.youcode.youquiz.common.controllers.GenericControllerImpl;
 import org.youcode.youquiz.dtos.quizAssignment.QuizAssignmentRequestDTO;
 import org.youcode.youquiz.dtos.quizAssignment.QuizAssignmentResponseDTO;
@@ -10,9 +15,15 @@ import org.youcode.youquiz.entities.embbedableId.QuizAssignmentId;
 import org.youcode.youquiz.services.QuizAssignmentService;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/quiz_assignments")
 public class QuizAssignmentController extends GenericControllerImpl<QuizAssignment, QuizAssignmentId, QuizAssignmentRequestDTO, QuizAssignmentResponseDTO> {
     public QuizAssignmentController(QuizAssignmentService service) {
         super(service);
+    }
+
+    @PostMapping("/quizzes/{quiz_id}")
+    public ResponseEntity<ApiResponse<QuizAssignmentResponseDTO>> create(@RequestBody @Valid QuizAssignmentRequestDTO request) {
+        QuizAssignmentResponseDTO responseDTO = service.create(request);
+        return ResponseEntity.ok(ApiResponse.success(responseDTO, "Item created successfully"));
     }
 }
--- src/main/java/org/youcode/youquiz/services/impl/QuizAssignmentServiceImpl.java
+++ src/main/java/org/youcode/youquiz/services/impl/QuizAssignmentServiceImpl.java
@@ -6,17 +6,44 @@ import org.springframework.validation.annotation.Validated;
 import org.youcode.youquiz.common.services.GenericServiceImpl;
 import org.youcode.youquiz.dtos.quizAssignment.QuizAssignmentRequestDTO;
 import org.youcode.youquiz.dtos.quizAssignment.QuizAssignmentResponseDTO;
+import org.youcode.youquiz.entities.Quiz;
 import org.youcode.youquiz.entities.QuizAssignment;
+import org.youcode.youquiz.entities.Student;
 import org.youcode.youquiz.entities.embbedableId.QuizAssignmentId;
 import org.youcode.youquiz.mappers.QuizAssignmentMapper;
 import org.youcode.youquiz.repositories.QuizAssignmentRepository;
+import org.youcode.youquiz.repositories.QuizRepository;
+import org.youcode.youquiz.repositories.StudentRepository;
 import org.youcode.youquiz.services.QuizAssignmentService;
 
 @Service
 @Transactional
 @Validated
 public class QuizAssignmentServiceImpl extends GenericServiceImpl<QuizAssignment, QuizAssignmentId, QuizAssignmentRequestDTO, QuizAssignmentResponseDTO> implements QuizAssignmentService {
-    public QuizAssignmentServiceImpl(QuizAssignmentRepository repository, QuizAssignmentMapper mapper) {
+
+    private final StudentRepository studentRepository;
+    private final QuizRepository quizRepository;
+    public QuizAssignmentServiceImpl(QuizAssignmentRepository repository, QuizAssignmentMapper mapper, StudentRepository studentRepository, QuizRepository quizRepository) {
         super(repository, mapper);
+        this.studentRepository = studentRepository;
+        this.quizRepository = quizRepository;
+    }
+
+    @Override
+    public QuizAssignmentResponseDTO create(QuizAssignmentRequestDTO requestDTO) {
+        Student existingStudent = studentRepository.findById(requestDTO.studentId())
+                .orElseThrow(() -> new RuntimeException("Student with Id " + requestDTO.studentId() + " not found"));
+
+        Quiz existingQuiz = quizRepository.findById(requestDTO.quizId())
+                .orElseThrow(() -> new RuntimeException("Quiz with Id " + requestDTO.quizId() + " not found"));
+
+        QuizAssignmentId quizAssignmentId = new QuizAssignmentId(existingStudent.getId(), existingQuiz.getId());
+        QuizAssignment quizAssignment = mapper.toEntity(requestDTO);
+        quizAssignment.setId(quizAssignmentId);
+        quizAssignment.setQuiz(existingQuiz);
+        quizAssignment.setStudent(existingStudent);
+
+        QuizAssignment saved = repository.save(quizAssignment);
+        return mapper.toDto(saved);
     }
 }
