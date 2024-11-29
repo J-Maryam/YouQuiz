ec0aaf48671113d756200cf80494a0366980fbb3
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-29 01:03:36 +0100

    feat: added annotation (exists and uniqueValue) in QuestionRequestDTO

--- src/main/java/org/youcode/youquiz/dtos/question/QuestionRequestDTO.java
+++ src/main/java/org/youcode/youquiz/dtos/question/QuestionRequestDTO.java
@@ -3,6 +3,11 @@ package org.youcode.youquiz.dtos.question;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.youcode.youquiz.common.validation.annotation.Exists;
+import org.youcode.youquiz.common.validation.annotation.UniqueValue;
+import org.youcode.youquiz.entities.Level;
+import org.youcode.youquiz.entities.Question;
+import org.youcode.youquiz.entities.Subject;
 import org.youcode.youquiz.entities.enums.QuestionType;
 
 public record QuestionRequestDTO(
@@ -14,6 +19,7 @@ public record QuestionRequestDTO(
         int numberOfCorrectAnswers,
 
         @NotBlank
+        @UniqueValue(entityClass = Question.class, fieldName = "text")
         String text,
 
         @NotNull
@@ -21,8 +27,11 @@ public record QuestionRequestDTO(
         QuestionType questionType,
 
         @NotNull
+        @Exists(entityClass = Subject.class, message = "Subject id does not exist")
         Long subjectId,
 
+        @NotNull
+        @Exists(entityClass = Level.class, message = "Level id does not exist")
         Long levelId
 ) {
 }
