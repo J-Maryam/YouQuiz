158315ebea2b8bd3f01e32eb894801ee5c95bd7e
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-01 15:39:11 +0100

    feat: added EmbeddableAnswerValidationDTO in AnswerMapper

--- src/main/java/org/youcode/youquiz/mappers/AnswerValidationMapper.java
+++ src/main/java/org/youcode/youquiz/mappers/AnswerValidationMapper.java
@@ -1,11 +1,20 @@
 package org.youcode.youquiz.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.youcode.youquiz.common.Mapper.GenericMapper;
 import org.youcode.youquiz.dtos.answerValidation.AnswerValidationRequestDTO;
 import org.youcode.youquiz.dtos.answerValidation.AnswerValidationResponseDTO;
+import org.youcode.youquiz.dtos.answerValidation.EmbeddableAnswerValidationDTO;
 import org.youcode.youquiz.entities.AnswerValidation;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {QuizMapper.class, StudentMapper.class, QuestionMapper.class, AnswerMapper.class})
 public interface AnswerValidationMapper extends GenericMapper<AnswerValidation, AnswerValidationRequestDTO, AnswerValidationResponseDTO> {
+
+    @Mapping(source = "quiz", target = "quiz")
+    @Mapping(source = "student", target = "student")
+    @Mapping(source = "answer", target = "answer")
+    @Mapping(source = "question", target = "question")
+    EmbeddableAnswerValidationDTO toEmbeddableAnswerValidationDTO(AnswerValidation answerValidation);
+
 }
