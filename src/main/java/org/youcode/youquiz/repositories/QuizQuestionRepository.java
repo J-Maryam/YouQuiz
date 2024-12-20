65db904289699d04b9fdda31309dfc642244d442
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-20 11:16:44 +0100

    feat: Added `existsByQuizIdAndQuestionId` method to check if a specific association between a quiz and a question exists in the repository.

--- src/main/java/org/youcode/youquiz/repositories/QuizQuestionRepository.java
+++ src/main/java/org/youcode/youquiz/repositories/QuizQuestionRepository.java
@@ -3,7 +3,9 @@ package org.youcode.youquiz.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youcode.youquiz.entities.QuizQuestion;
+import org.youcode.youquiz.entities.embbedableId.QuizQuestionId;
 
 @Repository
-public interface QuizQuestionRepository extends JpaRepository<QuizQuestion, Long> {
+public interface QuizQuestionRepository extends JpaRepository<QuizQuestion, QuizQuestionId> {
+    boolean existsByQuizIdAndQuestionId(Long quizId, Long questionId);
 }
