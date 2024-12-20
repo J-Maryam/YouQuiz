2ad89ce4a876317c68bdd8c4471939317ef9a794
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-20 11:27:36 +0100

    feat: Add getByQuestionId method to QuestionHasAnswersService
    - Introduced `getByQuestionId` method to retrieve all `QuestionHasAnswers` entries associated with a specific question.

--- src/main/java/org/youcode/youquiz/services/QuestionHasAnswersService.java
+++ src/main/java/org/youcode/youquiz/services/QuestionHasAnswersService.java
@@ -6,5 +6,8 @@ import org.youcode.youquiz.dtos.questionHasAnswers.QuestionHasAnswersResponseDTO
 import org.youcode.youquiz.entities.QuestionHasAnswers;
 import org.youcode.youquiz.entities.embbedableId.QuestionHasAnswersId;
 
+import java.util.List;
+
 public interface QuestionHasAnswersService extends GenericService<QuestionHasAnswers, QuestionHasAnswersId, QuestionHasAnswersRequestDTO, QuestionHasAnswersResponseDTO> {
+    List<QuestionHasAnswersResponseDTO> getByQuestionId(Long questionId);
 }
