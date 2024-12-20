c21ab618301b5de784e2a29ae7cac82e3eef74d1
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-20 11:01:58 +0100

    refactor: update ID type to `QuizQuestionId` for `QuizQuestionController`
    - Changed ID type from `Long` to `QuizQuestionId` in `QuizQuestionController`.
    
    - Adjusted the controller and service layers to use the new ID type.

--- src/main/java/org/youcode/youquiz/controllers/QuizQuestionController.java
+++ src/main/java/org/youcode/youquiz/controllers/QuizQuestionController.java
@@ -6,11 +6,12 @@ import org.youcode.youquiz.common.controllers.GenericControllerImpl;
 import org.youcode.youquiz.dtos.quizQuestion.QuizQuestionRequestDTO;
 import org.youcode.youquiz.dtos.quizQuestion.QuizQuestionResponseDTO;
 import org.youcode.youquiz.entities.QuizQuestion;
+import org.youcode.youquiz.entities.embbedableId.QuizQuestionId;
 import org.youcode.youquiz.services.QuizQuestionService;
 
 @RestController
 @RequestMapping("/api/quiz_questions")
-public class QuizQuestionController extends GenericControllerImpl<QuizQuestion, Long, QuizQuestionRequestDTO, QuizQuestionResponseDTO> {
+public class QuizQuestionController extends GenericControllerImpl<QuizQuestion, QuizQuestionId, QuizQuestionRequestDTO, QuizQuestionResponseDTO> {
     public QuizQuestionController(QuizQuestionService service) {
         super(service);
     }
