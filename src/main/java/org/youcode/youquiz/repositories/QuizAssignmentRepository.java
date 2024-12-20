7b9084dc3f725b40b9ed9827debfadc2a5f82935
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-20 11:15:53 +0100

    - Added `findByQuizIdAndStudentId` method to find a specific quiz assignment for a given quiz and student.

--- src/main/java/org/youcode/youquiz/repositories/QuizAssignmentRepository.java
+++ src/main/java/org/youcode/youquiz/repositories/QuizAssignmentRepository.java
@@ -2,13 +2,14 @@ package org.youcode.youquiz.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.youcode.youquiz.entities.Quiz;
 import org.youcode.youquiz.entities.QuizAssignment;
 import org.youcode.youquiz.entities.embbedableId.QuizAssignmentId;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface QuizAssignmentRepository extends JpaRepository<QuizAssignment, QuizAssignmentId> {
     List<QuizAssignment> findByQuizId(Long quizId);
+    Optional<QuizAssignment> findByQuizIdAndStudentId(Long quizId, Long studentId);
 }
