beca9c004a6d2d090a68112e8d1731c19abedfa9
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-20 11:05:01 +0100

    feat: add validation to `AnswerRequestDTO` for non-blank answer text
    - Added `@NotBlank` annotation to the `text` field in `AnswerRequestDTO` to enforce that the answer text cannot be blank.
    
    - This ensures that the answer provided in requests meets the required validation criteria.

--- src/main/java/org/youcode/youquiz/dtos/answer/AnswerRequestDTO.java
+++ src/main/java/org/youcode/youquiz/dtos/answer/AnswerRequestDTO.java
@@ -1,6 +1,9 @@
 package org.youcode.youquiz.dtos.answer;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AnswerRequestDTO(
+        @NotBlank(message = "Answer text cannot be blank. Please provide a valid answer.")
         String text
 ) {
 }
