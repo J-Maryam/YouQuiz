a36811340730ec267e473988b9ada0b2a23245fd
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-12-20 11:17:30 +0100

    feat: Added `findAllByQuizId` method to retrieve all subjects associated with a specific quiz.

--- src/main/java/org/youcode/youquiz/repositories/QuizSubjectRepository.java
+++ src/main/java/org/youcode/youquiz/repositories/QuizSubjectRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 import org.youcode.youquiz.entities.QuizSubject;
 import org.youcode.youquiz.entities.embbedableId.QuizSubjectId;
 
+import java.util.List;
+
 @Repository
 public interface QuizSubjectRepository extends JpaRepository<QuizSubject, QuizSubjectId> {
+    List<QuizSubject> findAllByQuizId(Long quizId);
 }
